100812d7820cf8ef1a730800e8909aca71e10b67
Temirkhan Sailau <temirkhansailau@Temirkhans-MacBook-Pro.local>
2017-11-10 19:48:57 +0600

    [Add]-added faculties and specialties in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 DatabaseCleaner.clean_with(:truncation)
 
-
+Industry
 fields = ['Маркетинг', 'Информационные технологии', 'Бухгалтерский учёт', 'Администрация', 
           'Рекламное дело', 'Банкинг', 'Общественные услуги', 'Строительство', 'Консалтинг', 
           'Дизайн', 'Образование', 'Фермерское дело', 'Государственная служба', 'Здравохранение', 
@@ -10,6 +10,20 @@ fields.each {
      |i| Industry.create(name: i)
 }
 
+falculties = ['Инженерии и естественных наук','Юриспруденции и соц.-гум. наук','Педогогических и гуманитарных наук','Бизнес-школа СДУ']
+
+specialties2 = ['Юриспруденция','Международные отношения','Международное право','История','Журналистика']
+specialties3 = ['Филология','Переводческое дело','Иностранный язык: два иностранных языка','Казахский язык и литература','Иностранная филология','Физика - Информатика (педагогическая)','Химия - Биология (педагогическая)','Математика (педагогическая)']
+specialties1 = ['Информационные системы','Вычислительная техника и программное обеспечение','Математика (научная)']
+specialties4 = ['Учет и аудит','Финансы','Маркетинг','Экономика','Менеджмент']
+
+falculties.each{|i| Falculty.create(name: i)}
+specialties1.each{|i| Specialty.create(name: i , falculty_id: 1)}
+specialties2.each{|i| Specialty.create(name: i , falculty_id: 2)}
+specialties3.each{|i| Specialty.create(name: i , falculty_id: 3)}
+specialties4.each{|i| Specialty.create(name: i , falculty_id: 4)}
+
+User.destroy_all
 User.create(
     email:"zshanabek@gmail.com", password:"123456789",password_confirmation:"123456789", 
     first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
@@ -22,7 +36,7 @@ User.create(
         graduation_date: Faker::Date.between(17.years.ago, Date.today)
     )
 end
-
+Vacancy.destroy_all
 20.times do
     Vacancy.create(
         title: Faker::Job.title, salary: Faker::Number.decimal(2), 
